64be5d48d8ce015257dd379e9f46b10234a33415
Tanmay Gupta <guptatanmay1008@gmail.com>
2025-04-18 12:51:50 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,45 @@ import App from "./App.jsx";
 import "./styles/index.css";
 import { BrowserRouter } from "react-router-dom";
 
+
+// filepath: g:\College Codes\Semester 6\ip_mini\src\main.jsx
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker.register("/service-worker.js").then(
+//       (registration) => {
+//         console.log("Service Worker registered with scope:", registration.scope);
+//       },
+//       (error) => {
+//         console.error("Service Worker registration failed:", error);
+//       }
+//     );
+//   });
+// }
+
+
+if ("serviceWorker" in navigator && import.meta.env.PROD) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").then(
+      (registration) => {
+        console.log("Service Worker registered with scope:", registration.scope);
+      },
+      (error) => {
+        console.error("Service Worker registration failed:", error);
+      }
+    );
+  });
+}
+
+// // main.jsx or index.jsx or App.jsx
+// if ('serviceWorker' in navigator && import.meta.env.PROD) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/sw.js') // your service worker path
+//       .then(reg => console.log('SW registered:', reg))
+//       .catch(err => console.log('SW registration failed:', err));
+//   });
+// }
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // import { VitePWA } from "vite-plugin-pwa";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react()
+//     // VitePWA()
+//   ],
+// });
+
+
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Tea of Assam",
+        short_name: "TOA",
+        description: "A Sip of Authenticity, Directly from the Tea Gardens to Your Cup.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/src/assets/favicon.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+          },
+        ],
+      },
+    }),
+  ],
 });
