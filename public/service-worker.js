e0da00846c0bcbb538bcbbfdc96ffc3296530642
Tanmay Gupta <guptatanmay1008@gmail.com>
2025-04-18 15:59:21 +0530

    fifth commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,46 @@
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-      caches.open("tea-of-assam-cache").then((cache) => {
-        return cache.addAll([
-          "/",
-          "/index.html",
-          "/src/assets/favicon.svg",
-          "/src/styles/index.css",
-          "/src/main.jsx",
-        ]);
-      })
-    );
-  });
+// self.addEventListener("install", (event) => {
+//     event.waitUntil(
+//       caches.open("tea-of-assam-cache").then((cache) => {
+//         return cache.addAll([
+//           "/",
+//           "/index.html",
+//           "/src/assets/favicon.svg",
+//           "/src/styles/index.css",
+//           "/src/main.jsx",
+//         ]);
+//       })
+//     );
+//   });
   
-  self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
+//   self.addEventListener("fetch", (event) => {
+//     event.respondWith(
+//       caches.match(event.request).then((response) => {
+//         return response || fetch(event.request);
+//       })
+//     );
+//   });
+
+
+
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("tea-of-assam-cache").then((cache) => {
+      return cache.addAll([
+        "/vite-host/",
+        "/vite-host/index.html",
+        "/vite-host/src/assets/favicon.svg",
+        "/vite-host/src/styles/index.css",
+        "/vite-host/src/main.jsx",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
